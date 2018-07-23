8190e5cae418fef3cf1c0f5613195a41e34cc116
MarcinFedorek <fedorek87@gmail.com>
2018-07-23 18:36:16 +0200

    person

--- src/main/java/com/example/demo/PersonAdder.java
+++ src/main/java/com/example/demo/PersonAdder.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,16 +11,23 @@ import java.util.List;
 @RestController
 public class PersonAdder {
 
+
+    @Autowired
+    private PersonRepo personRepo;
+
     private List<String> stringList = new ArrayList<>();
 
     @RequestMapping("/add")
     public void add(@RequestParam("name") String name) {
+
+        Person p = new Person();
+        p.setName(name);
         stringList.add(name);
     }
 
     @RequestMapping("/get")
-    public List<String> get(){
-        return stringList;
+    public List<Person> get(){
+        return personRepo.findAll();
     }
 
 
